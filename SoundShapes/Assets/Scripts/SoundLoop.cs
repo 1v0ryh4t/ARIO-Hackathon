9a37f48693af9615fdf873fc971af1c9ace2d6e7
Eric Reed <septem216@gmail.com>
2017-04-11 10:49:12 -0400

    Sound Project v1.0
    Added blue sphere that plays C3 when clicked. Need to add more shapes
    that play multiple pitches.

---
+++ SoundShapes/Assets/Scripts/SoundLoop.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundLoop : MonoBehaviour {
+    public AudioClip bloop;
+	// Use this for initialization
+	void Start () {
+        this.gameObject.AddComponent<AudioSource>();
+        this.GetComponent<AudioSource>().clip = bloop;
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        // If the left mouse button is pressed down...
+        if (Input.GetMouseButtonDown(0) == true)
+        {
+            GetComponent<AudioSource>().loop = true;
+            GetComponent<AudioSource>().Play();
+        }
+        // If the left mouse button is released...
+        if (Input.GetMouseButtonUp(0) == true)
+        {
+            GetComponent<AudioSource>().Stop();
+        }
+    }
+}
---
+++ SoundShapes/Assets/Scripts/TalkingShape.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkingShape : MonoBehaviour {
+    public AudioClip bloop;
+    // Use this for initialization
+    void Start()
+    {
+        this.gameObject.AddComponent<AudioSource>();
+        this.GetComponent<AudioSource>().clip = bloop;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        { // if left button pressed...
+            Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                // If the left mouse button is pressed down...
+                GetComponent<AudioSource>().loop = true;
+                GetComponent<AudioSource>().Play();
+            }
+            // If the left mouse button is released...
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+           GetComponent<AudioSource>().Stop();
+        }
+    }
+}
