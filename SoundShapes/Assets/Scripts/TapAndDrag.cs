b0363302646d8a4d77705057bb7cf22955657482
Zachary Helm <zac1790@gmail.com>
2017-04-13 01:19:21 -0400

    Basic Tap&Drag Script With LoopPad Inherited Class
    Basic Dragging vs Tap and the loop-pad that inherits from Tap&Drag and
    overrides OnTap. Also, I found the Audio Mixer, it's under the "Window"
    submenu at the top of Unity. Added a main mixer. We can also add effects
    like underwater via Reverb Zones that are enabled or disabled by a
    controller.

--- SoundShapes/Assets/Scripts/TapAndDrag.cs
+++ SoundShapes/Assets/Scripts/TapAndDrag.cs
@@ -4,13 +4,63 @@ using UnityEngine;
 
 public class TapAndDrag : MonoBehaviour {
 
+    public float timeToDrag = 2.0f; // easy access to get the right feel
+    public float distToDrag = 32f; // same, but for distance dragged in pixels
+
+    private float dragTime;
+    private bool dragging;
+    private Vector2 clickPos;
+    private float distToCam;
+
 	// Use this for initialization
 	void Start () {
-		
+        dragging = false;
+        dragTime = 0.0f;
+
+  
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public virtual void OnTap ()
+    {
+        Debug.Log("Tapped");
+    }
+
+    private void OnMouseDown()
+    {
+        dragging = false;
+        dragTime = 0.0f;
+        clickPos = Input.mousePosition;
+    }
+
+    private void OnMouseDrag()
+    {
+        dragTime += Time.deltaTime;
+        if(dragging)
+        {
+            transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distToCam));
+        }
+        else
+        {
+            if (dragTime >= timeToDrag || Vector2.Distance(clickPos, Input.mousePosition) >= distToDrag)
+            {
+                dragging = true;
+                distToCam = Vector3.Distance(transform.position, Camera.main.transform.position); // get distance to do screen-based translation
+            }
+                
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        if(!dragging)
+        {
+            OnTap();
+        }
+
+    }
 }
