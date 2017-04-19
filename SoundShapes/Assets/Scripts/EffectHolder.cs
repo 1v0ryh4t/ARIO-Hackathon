6df9e4c23a8c455c06475d8cf121a28544297547
Zach Helm <zhelm@greenmeteorteam.com>
2017-04-19 17:18:43 -0400

    6 Loops from Garage Band
    Probably not legal to ship and haven't thoroughly tested how in-sync
    they are.

---
+++ SoundShapes/Assets/Scripts/EffectHolder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectHolder : TapAndDrag {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void OnTap()
+    {
+        GameObject myEffect = transform.GetChild(0).gameObject;
+        myEffect.SetActive(!myEffect.activeSelf);
+        //base.OnTap();
+    }
+}
